a4ccbf51d376dba0a52efff0ed8b7f4842f54c3a
topelearnseverday35 <enochtopeonline@gmail.com>
2025-04-25 05:08:52 +0100

    Project setup

---
+++ oakcollectionsadmin/src/main/java/com/helium/oakcollectionsadmin/OakcollectionsadminApplication.java
@@ -0,0 +1,13 @@
+package com.helium.oakcollectionsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakcollectionsadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OakcollectionsadminApplication.class, args);
+	}
+
+}
---
+++ oakcollectionsadmin/src/test/java/com/helium/oakcollectionsadmin/OakcollectionsadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helium.oakcollectionsadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OakcollectionsadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
