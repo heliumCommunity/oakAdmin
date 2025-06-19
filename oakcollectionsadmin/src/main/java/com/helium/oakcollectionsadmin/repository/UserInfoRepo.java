53069df4d503a14cd518a10a61fdb468c256a09c
topelearnseverday35 <enochtopeonline@gmail.com>
2025-06-19 20:42:18 +0100

    "fixed db error"

--- oakcollectionsadmin/src/main/java/com/helium/oakcollectionsadmin/repository/UserInfoRepo.java
+++ oakcollectionsadmin/src/main/java/com/helium/oakcollectionsadmin/repository/UserInfoRepo.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserInfoRepo extends JpaRepository<UserInfo, String> {
+public interface UserInfoRepo extends JpaRepository<UserInfo, Long> {
     Optional<UserInfo> findByEmail(String email);
     UserInfo findByStaffId(String staffId);
 
